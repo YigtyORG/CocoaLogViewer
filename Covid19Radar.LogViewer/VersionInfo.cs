9daf2e5fb5554dc111bdb61ec5d4196954b5c5a3
Takym <15681312+Takym@users.noreply.github.com>
2021-06-06 03:09:15 +0900

    VersionInfo.cs 更新

--- Covid19Radar.LogViewer/VersionInfo.cs
+++ Covid19Radar.LogViewer/VersionInfo.cs
@@ -6,6 +6,7 @@
  * distributed under the MIT License.
 ****/
 
+using System;
 using System.Reflection;
 using Covid19Radar.LogViewer.Globalization;
 
@@ -21,43 +22,47 @@ namespace Covid19Radar.LogViewer
 
 		public static string GetCaption()
 		{
-			return GetCaption(Assembly.GetExecutingAssembly());
-		}
-
-		public static string GetCaption(Assembly asm)
-		{
 			if (_caption is null) {
-				string  unknown = LanguageData.Current.VersionInfo_Unknown;
-				string? name    = asm.GetCustomAttribute<AssemblyProductAttribute>()?.Product;
-				string  v       = asm.GetName().Version?.ToString(4) ?? "?.?.?.?";
-				string  cn      = asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? unknown;
-				string  bc      = asm.GetCustomAttribute<AssemblyConfigurationAttribute>()?.Configuration ?? unknown;
-				switch (bc) {
-				case Debug:
-					_caption = $"{name} - {LanguageData.Current.VersionInfo_Debug} [v{v}, cn:{cn}]";
-					break;
-				case Release:
-					_caption = $"{name} [v{v}, cn:{cn}]";
-					break;
-				default:
-					_caption = $"{name} [v{v}, cn:{cn}, bc:{bc}]";
-					break;
-				}
+				_caption = GetCaption(Assembly.GetExecutingAssembly());
 			}
 			return _caption;
 		}
 
+		public static string GetCaption(Assembly asm)
+		{
+			return GetCaption(
+				asm.GetCustomAttribute<AssemblyProductAttribute>()?.Product,
+				asm.GetName().Version,
+				asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion,
+				asm.GetCustomAttribute<AssemblyConfigurationAttribute>()?.Configuration
+			);
+		}
+
+		public static string GetCaption(string? name, Version? version, string? codeName, string? buildConfiguration)
+		{
+			string  unknown = LanguageData.Current.VersionInfo_Unknown;
+			string  v       = version?.ToString(4) ?? "?.?.?.?";
+			string  cn      = codeName             ?? unknown;
+			string  bc      = buildConfiguration   ?? unknown;
+
+			return bc switch {
+				Debug   => $"{name} - {LanguageData.Current.VersionInfo_Debug} [v{v}, cn:{cn}]",
+				Release => $"{name} [v{v}, cn:{cn}]",
+				_       => $"{name} [v{v}, cn:{cn}, bc:{bc}]"
+			};
+		}
+
 		public static string GetCopyright()
 		{
-			return GetCopyright(Assembly.GetExecutingAssembly());
+			if (_copyright is null) {
+				_copyright = GetCopyright(Assembly.GetExecutingAssembly());
+			}
+			return _copyright;
 		}
 
 		public static string GetCopyright(Assembly asm)
 		{
-			if (_copyright is null) {
-				_copyright = asm.GetCustomAttribute<AssemblyCopyrightAttribute>()?.Copyright ?? LanguageData.Current.VersionInfo_Unknown;
-			}
-			return _copyright;
+			return asm.GetCustomAttribute<AssemblyCopyrightAttribute>()?.Copyright ?? LanguageData.Current.VersionInfo_Unknown;
 		}
 	}
 }
