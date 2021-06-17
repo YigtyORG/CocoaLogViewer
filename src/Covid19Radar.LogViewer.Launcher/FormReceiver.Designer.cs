305a7de41f7826603d6cabff0ea3fae5384e341e
Takym <15681312+Takym@users.noreply.github.com>
2021-06-17 11:36:53 +0900

    FormReceiver のデザインを設定

--- src/Covid19Radar.LogViewer.Launcher/FormReceiver.Designer.cs
+++ src/Covid19Radar.LogViewer.Launcher/FormReceiver.Designer.cs
@@ -35,12 +35,54 @@ namespace Covid19Radar.LogViewer.Launcher
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.labelDescription = new System.Windows.Forms.Label();
+			this.textBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// labelDescription
+			// 
+			this.labelDescription.AutoSize = true;
+			this.labelDescription.Font = new System.Drawing.Font("Yu Gothic UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.labelDescription.Location = new System.Drawing.Point(8, 8);
+			this.labelDescription.Name = "labelDescription";
+			this.labelDescription.Size = new System.Drawing.Size(255, 45);
+			this.labelDescription.TabIndex = 0;
+			this.labelDescription.Text = "labelDescription";
+			// 
+			// textBox
+			// 
+			this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.textBox.Location = new System.Drawing.Point(8, 64);
+			this.textBox.Multiline = true;
+			this.textBox.Name = "textBox";
+			this.textBox.ReadOnly = true;
+			this.textBox.Size = new System.Drawing.Size(480, 208);
+			this.textBox.TabIndex = 1;
+			this.textBox.Text = "textBox";
+			// 
+			// FormReceiver
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(496, 281);
+			this.Controls.Add(this.textBox);
+			this.Controls.Add(this.labelDescription);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(512, 320);
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(512, 320);
+			this.Name = "FormReceiver";
 			this.Text = "FormReceiver";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label labelDescription;
+		private System.Windows.Forms.TextBox textBox;
 	}
 }
