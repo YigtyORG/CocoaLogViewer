7ea8aa67e83b5283ce69f63e644ca68626056c7d
Takym (たかやま) <15681312+Takym@users.noreply.github.com>
2021-06-22 15:32:20 +0900

    Update Program.cs

--- src/Covid19Radar.LogViewer.Launcher/Program.cs
+++ src/Covid19Radar.LogViewer.Launcher/Program.cs
@@ -50,7 +50,7 @@ namespace Covid19Radar.LogViewer.Launcher
 
 		private static void HandleException(Exception exception)
 		{
-			Debug.Fail(exception.ToString());
+			Debug.Fail(exception.Message, exception.ToString());
 			MessageBox.Show(exception.Message, LanguageData.Current.MainWindow_OFD_Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
 
 			using (var fs = new FileStream(Path.Combine(AppContext.BaseDirectory, "error_log.md"), FileMode.Append, FileAccess.Write, FileShare.None))
